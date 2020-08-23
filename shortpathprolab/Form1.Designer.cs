2bb9ef6d00b4737b60a87e1ff2190b0c867fa399
Cihat Özdemir <cihatozdemirmis@hotmail.com>
2020-08-24 00:34:58 +0300

    version1.0

---
+++ shortpathprolab/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace shortpathprolab
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.FindPathBtn = new System.Windows.Forms.Button();
+            this.CityList = new System.Windows.Forms.ListBox();
+            this.CityNames = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.addcitytolistBtn = new System.Windows.Forms.Button();
+            this.AddCityPanel = new System.Windows.Forms.Panel();
+            this.RemoveCityToListBtn = new System.Windows.Forms.Button();
+            this.DrawPathBorder = new System.Windows.Forms.Panel();
+            this.BackBtn = new System.Windows.Forms.Button();
+            this.MapImage = new System.Windows.Forms.PictureBox();
+            this.VisitedCityList = new System.Windows.Forms.ListBox();
+            this.DrawTimer = new System.Windows.Forms.Timer(this.components);
+            this.AddCityPanel.SuspendLayout();
+            this.DrawPathBorder.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MapImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // FindPathBtn
+            // 
+            this.FindPathBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FindPathBtn.Location = new System.Drawing.Point(27, 366);
+            this.FindPathBtn.Name = "FindPathBtn";
+            this.FindPathBtn.Size = new System.Drawing.Size(246, 84);
+            this.FindPathBtn.TabIndex = 0;
+            this.FindPathBtn.Text = "YOLU BUL";
+            this.FindPathBtn.UseVisualStyleBackColor = true;
+            this.FindPathBtn.Click += new System.EventHandler(this.FindPathBtn_Click);
+            // 
+            // CityList
+            // 
+            this.CityList.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.CityList.FormattingEnabled = true;
+            this.CityList.ItemHeight = 20;
+            this.CityList.Location = new System.Drawing.Point(27, 96);
+            this.CityList.Name = "CityList";
+            this.CityList.Size = new System.Drawing.Size(246, 224);
+            this.CityList.TabIndex = 1;
+            // 
+            // CityNames
+            // 
+            this.CityNames.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.CityNames.FormattingEnabled = true;
+            this.CityNames.Location = new System.Drawing.Point(27, 44);
+            this.CityNames.Name = "CityNames";
+            this.CityNames.Size = new System.Drawing.Size(181, 33);
+            this.CityNames.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(65, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ŞEHİRLER";
+            // 
+            // addcitytolistBtn
+            // 
+            this.addcitytolistBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.addcitytolistBtn.Location = new System.Drawing.Point(215, 44);
+            this.addcitytolistBtn.Name = "addcitytolistBtn";
+            this.addcitytolistBtn.Size = new System.Drawing.Size(58, 33);
+            this.addcitytolistBtn.TabIndex = 4;
+            this.addcitytolistBtn.Text = "EKLE";
+            this.addcitytolistBtn.UseVisualStyleBackColor = true;
+            this.addcitytolistBtn.Click += new System.EventHandler(this.addcitytolistBtn_Click);
+            // 
+            // AddCityPanel
+            // 
+            this.AddCityPanel.Controls.Add(this.RemoveCityToListBtn);
+            this.AddCityPanel.Controls.Add(this.label1);
+            this.AddCityPanel.Controls.Add(this.addcitytolistBtn);
+            this.AddCityPanel.Controls.Add(this.FindPathBtn);
+            this.AddCityPanel.Controls.Add(this.CityList);
+            this.AddCityPanel.Controls.Add(this.CityNames);
+            this.AddCityPanel.Location = new System.Drawing.Point(0, 0);
+            this.AddCityPanel.Name = "AddCityPanel";
+            this.AddCityPanel.Size = new System.Drawing.Size(300, 470);
+            this.AddCityPanel.TabIndex = 5;
+            // 
+            // RemoveCityToListBtn
+            // 
+            this.RemoveCityToListBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.RemoveCityToListBtn.Location = new System.Drawing.Point(27, 327);
+            this.RemoveCityToListBtn.Name = "RemoveCityToListBtn";
+            this.RemoveCityToListBtn.Size = new System.Drawing.Size(246, 33);
+            this.RemoveCityToListBtn.TabIndex = 5;
+            this.RemoveCityToListBtn.Text = "SEÇİLİ ŞEHRİ LİSTEDEN KALDIR";
+            this.RemoveCityToListBtn.UseVisualStyleBackColor = true;
+            this.RemoveCityToListBtn.Click += new System.EventHandler(this.RemoveCityToListBtn_Click);
+            // 
+            // DrawPathBorder
+            // 
+            this.DrawPathBorder.Controls.Add(this.BackBtn);
+            this.DrawPathBorder.Controls.Add(this.MapImage);
+            this.DrawPathBorder.Controls.Add(this.VisitedCityList);
+            this.DrawPathBorder.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DrawPathBorder.Location = new System.Drawing.Point(0, 0);
+            this.DrawPathBorder.Name = "DrawPathBorder";
+            this.DrawPathBorder.Size = new System.Drawing.Size(1179, 566);
+            this.DrawPathBorder.TabIndex = 6;
+            this.DrawPathBorder.Visible = false;
+            // 
+            // BackBtn
+            // 
+            this.BackBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BackBtn.Location = new System.Drawing.Point(4, 4);
+            this.BackBtn.Name = "BackBtn";
+            this.BackBtn.Size = new System.Drawing.Size(98, 32);
+            this.BackBtn.TabIndex = 3;
+            this.BackBtn.Text = "GERİ";
+            this.BackBtn.UseVisualStyleBackColor = true;
+            this.BackBtn.Click += new System.EventHandler(this.BackBtn_Click);
+            // 
+            // MapImage
+            // 
+            this.MapImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MapImage.Image = ((System.Drawing.Image)(resources.GetObject("MapImage.Image")));
+            this.MapImage.Location = new System.Drawing.Point(0, 0);
+            this.MapImage.Name = "MapImage";
+            this.MapImage.Size = new System.Drawing.Size(1006, 566);
+            this.MapImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.MapImage.TabIndex = 0;
+            this.MapImage.TabStop = false;
+            // 
+            // VisitedCityList
+            // 
+            this.VisitedCityList.Dock = System.Windows.Forms.DockStyle.Right;
+            this.VisitedCityList.FormattingEnabled = true;
+            this.VisitedCityList.Location = new System.Drawing.Point(1006, 0);
+            this.VisitedCityList.Name = "VisitedCityList";
+            this.VisitedCityList.Size = new System.Drawing.Size(173, 566);
+            this.VisitedCityList.TabIndex = 1;
+            // 
+            // DrawTimer
+            // 
+            this.DrawTimer.Interval = 600;
+            this.DrawTimer.Tick += new System.EventHandler(this.DrawTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1179, 566);
+            this.Controls.Add(this.DrawPathBorder);
+            this.Controls.Add(this.AddCityPanel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kargo Yol Uygulaması";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.AddCityPanel.ResumeLayout(false);
+            this.AddCityPanel.PerformLayout();
+            this.DrawPathBorder.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.MapImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button FindPathBtn;
+        private System.Windows.Forms.ListBox CityList;
+        private System.Windows.Forms.ComboBox CityNames;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button addcitytolistBtn;
+        private System.Windows.Forms.Panel AddCityPanel;
+        private System.Windows.Forms.Panel DrawPathBorder;
+        private System.Windows.Forms.PictureBox MapImage;
+        private System.Windows.Forms.ListBox VisitedCityList;
+        private System.Windows.Forms.Button RemoveCityToListBtn;
+        private System.Windows.Forms.Timer DrawTimer;
+        private System.Windows.Forms.Button BackBtn;
+    }
+}
+
---
+++ shortpathprolab/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace shortpathprolab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            g = MapImage.CreateGraphics();
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            pen = new Pen(drawcolor, 3);
+            pen.StartCap = pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+        }
+
+        public struct Position
+        {
+            public double x;
+            public double y;
+        }
+
+        Random rastgele = new Random();
+        string[] cityname = new string[81];
+        Position[] citypositions = new Position[81];
+        double[,] SolvedCityDistance = new double[81, 81];
+        double[,] finaluzaklik;
+        private Graphics g;
+        Color drawcolor = new Color();
+        private Pen pen;
+        List<int> visiting = new List<int>();
+        List<int> visitcompleted = new List<int>();
+
+        private int FindCityPlate(string name)
+        {
+            for (int i = 0; i < 81; i++)
+                if (name == cityname[i])
+                    return i;
+
+            return -1;
+        }
+
+        void Dijkstra(double[,] cost, int startnode, int endnode)
+        {
+            double[] distance = new double[81];
+            double[] pred = new double[81];
+            bool[] visited = new bool[81];
+            int count, nextnode = 0, i, j;
+            double mindistance;
+
+            for (i = 0; i < 81; i++)
+            {
+                distance[i] = cost[startnode, i];
+                pred[i] = startnode;
+                visited[i] = false;
+            }
+
+            distance[startnode] = 0;
+            visited[startnode] = true;
+            count = 1;
+            while (count < 80)
+            {
+                mindistance = double.PositiveInfinity;
+
+                for (i = 0; i < 81; i++)
+                    if (distance[i] < mindistance && !visited[i])
+                    {
+                        mindistance = distance[i];
+                        nextnode = i;
+                    }
+
+                visited[nextnode] = true;
+
+                for (i = 0; i < 81; i++)
+                    if (!visited[i])
+                        if (mindistance + cost[nextnode, i] < distance[i])
+                        {
+                            distance[i] = mindistance + cost[nextnode, i];
+                            pred[i] = nextnode;
+                        }
+                count++;
+            }
+
+            j = endnode;
+
+            List<int> temp = new List<int>();
+            temp.Add(j);
+            do
+            {
+                j = Convert.ToInt32(pred[j]);
+                temp.Add(j);
+            } while (j != startnode);
+            for (int l = temp.Count - 2; l >= 0; l--)
+                visitcompleted.Add(temp[l]);
+
+        }
+
+        //TÜM ŞEHİRLERİN BİRBİRLERİNE OLAN UZAKLIĞI
+        public static void Solve(ref double[,] matrix)
+        {
+            for (int i = 0; i < 81; i++)
+            {
+                for (int j = 0; j < 81; j++)
+                {
+                    for (int k = 0; k < 81; k++)
+                    {
+                        matrix[j, k] = Math.Min(matrix[j, k], matrix[j, i] + matrix[i, k]);
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // ŞEHİR EKLEME PANELİNİ MERKEZE AL
+            AddCityPanel.Location = new Point(this.Width / 2 - 150, this.Height / 2 - 235);
+
+            string[] lines = File.ReadAllLines(@"komsu.txt");
+
+            //HARİTA ÜZERİNDEKİ KONUMLARI ÇEK
+            string[] positionlines = File.ReadAllLines(@"cityposition.txt");
+
+            for (int i = 0, y = 0; i < 81; i++, y += 2)
+            {
+                citypositions[i].x = Convert.ToDouble(positionlines[y]);
+                citypositions[i].y = Convert.ToDouble(positionlines[y + 1]);
+            }
+
+            string[] uzaklıklar = File.ReadAllLines(@"mesafe.txt");
+            finaluzaklik = new double[81, 81];
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                cityname[i] = lines[i].Split(',')[0];
+                CityNames.Items.Add(cityname[i]);
+            }
+
+            CityNames.SelectedIndex = 0;
+
+            for (int i = 0; i < 81; i++)
+            {
+                for (int j = 0; j < 81; j++)
+                {
+                    finaluzaklik[i, j] = double.PositiveInfinity;
+                }
+                string[] sehirler = lines[i].Split(',');
+                string[] tempuzaklik = uzaklıklar[i].Split(' ');
+                for (int k = 0; k < sehirler.Length; k++)
+                {
+                    finaluzaklik[i, FindCityPlate(sehirler[k])] = Convert.ToDouble(tempuzaklik[k]);
+                }
+            }
+
+            for (int i = 0; i < 81; i++)
+                for (int j = 0; j < 81; j++)
+                    SolvedCityDistance[i, j] = finaluzaklik[i, j];
+
+            //ŞEHİRLERİN BİRBİRLERİNE OLAN UZAKLIKLARINI BUL
+            Solve(ref SolvedCityDistance);
+
+        }
+
+        private void addcitytolistBtn_Click(object sender, EventArgs e)
+        {
+            if (Array.IndexOf(cityname, CityNames.SelectedItem) > -1 && visiting.Count < 10 && !visiting.Contains(CityNames.SelectedIndex) && CityNames.SelectedIndex != 40)
+            {
+                CityList.Items.Add(CityNames.SelectedItem);
+                visiting.Add(CityNames.SelectedIndex);
+            }
+        }
+
+        private void RemoveCityToListBtn_Click(object sender, EventArgs e)
+        {
+            int selectedindex = CityList.SelectedIndex;
+            if(selectedindex != -1)
+            {
+                CityList.Items.RemoveAt(selectedindex);
+                visiting.RemoveAt(selectedindex);
+            }
+        }
+
+        int selectedcity;
+        int nextcity;
+        double mindistance;
+
+        private void FindPathBtn_Click(object sender, EventArgs e)
+        {
+            AddCityPanel.Visible = false;
+            DrawPathBorder.Visible = true;
+            drawcolor = Color.FromArgb(255,rastgele.Next(0, 255), rastgele.Next(0, 255), rastgele.Next(0, 255));
+            pen.Color = drawcolor;
+            VisitedCityList.Items.Add("*** GÜZERGAH ***");
+            visitcompleted.Add(40);
+            selectedcity = 40;
+            int n = visiting.Count;
+
+            //******** EN KISASINI BUL **********//
+            for (int j = 0;j < n; j++)
+            {
+                mindistance = double.PositiveInfinity;
+                for (int i = 0; i < visiting.Count; i++)
+                {
+                    if (SolvedCityDistance[selectedcity, visiting[i]] < mindistance)
+                    {
+                        mindistance = SolvedCityDistance[selectedcity, visiting[i]];
+                        nextcity = visiting[i];
+                    }
+                }
+
+                Dijkstra(finaluzaklik, selectedcity, nextcity);
+                visiting.Remove(nextcity);
+                selectedcity = nextcity;
+            }
+
+            Dijkstra(finaluzaklik, selectedcity, 40);
+
+            for (int i = 0; i < visitcompleted.Count; i++)
+            {
+                VisitedCityList.Items.Add(cityname[visitcompleted[i]]);
+            }
+            counter = 0;
+            DrawTimer.Start();
+        }
+        int counter;
+        private void DrawTimer_Tick(object sender, EventArgs e)
+        {
+            g.DrawLine(pen, Convert.ToInt64(citypositions[visitcompleted[counter]].x * MapImage.Width), Convert.ToInt64(citypositions[visitcompleted[counter]].y * MapImage.Height), Convert.ToInt64(citypositions[visitcompleted[counter + 1]].x * MapImage.Width), Convert.ToInt64(citypositions[visitcompleted[counter + 1]].y * MapImage.Height));
+            counter++;
+            VisitedCityList.SelectedIndex = counter + 1;
+            if (CityList.Items.Contains(cityname[visitcompleted[counter]]))
+            {
+                g.DrawString(cityname[visitcompleted[counter]], new Font("Arial", 12, FontStyle.Bold),new SolidBrush(drawcolor), new Point(Convert.ToInt32(citypositions[visitcompleted[counter]].x * MapImage.Width), Convert.ToInt32(citypositions[visitcompleted[counter]].y * MapImage.Height - 20)));
+                g.DrawRectangle(pen, Convert.ToInt32(citypositions[visitcompleted[counter]].x * MapImage.Width) - 3, Convert.ToInt32(citypositions[visitcompleted[counter]].y * MapImage.Height) - 3, 6, 6);
+                drawcolor = Color.FromArgb(255,rastgele.Next(0, 255), rastgele.Next(0, 255), rastgele.Next(0, 255));
+                pen.Color = drawcolor;
+            }
+
+            if (counter == visitcompleted.Count- 1)
+                DrawTimer.Stop();
+        }
+
+        private void BackBtn_Click(object sender, EventArgs e)
+        {
+            DrawTimer.Stop();
+            CityList.Items.Clear();
+            VisitedCityList.Items.Clear();
+            visiting.Clear();
+            visitcompleted.Clear();
+            DrawPathBorder.Visible = false;
+            AddCityPanel.Visible = true;
+        }
+    }
+}
